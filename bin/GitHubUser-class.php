877f0ea1cd4a91dee58e580dfdee85bbe91ac481
Deiv <44372336+MrDeiv@users.noreply.github.com>
2018-10-27 20:02:40 +0200

    GitHubUser class
    Useful to get info about GitHub user.
    Designed to create a dev team page or similar.
    Uploaded new version with get() method for private fields.

---
+++ bin/GitHubUser-class.php
@@ -0,0 +1,62 @@
+<?php
+
+	/*
+		GitHubUser class
+	*/
+
+	class GitHubUser {
+	
+		private $login;
+		private $id;
+		private $avatar;
+		private $url;
+		private $name;
+		private $bio;
+		private $followers;
+		const userUrl="https://api.github.com/users/";
+
+		//constructor
+
+		public function __construct($userID) {
+			$temp= urlencode(userUrl+$userID);
+			$ris= json_decode(file_get_contents($temp),true);
+			if (isset($ris['login'])) $this->login= $ris['login'];
+			if (isset($ris['id'])) $this->id= $ris['id'];
+			if (isset($ris['avatar'])) $this->avatar= $ris['avatar'];
+			if (isset($ris['url'])) $this->url= $ris['url'];
+			if (isset($ris['name'])) $this->name= $ris['name'];
+			if (isset($ris['bio'])) $this->login= $ris['bio'];
+			if (isset($ris['followers'])) $this->followers= $ris['followers'];
+		}
+
+		public function getLogin() {
+			return $this->login;
+		}
+
+		public function getId() {
+			return $this->id;
+		}
+
+		public function getAvatar() {
+			return $this->avatar;
+		}
+
+		public function getUrl() {
+			return $this->url;
+		}
+
+		public function getName() {
+			return $this->name;
+		}
+
+		public function getBio() {
+			return $this->bio;
+		}
+
+		public function getFollowers() {
+			return $this->followers;
+		}
+
+	}
+
+?>
