292552f62e3d6aacc2da9b1496b0d52b5a85cc76
Damiano Casula <damiano@casula.me>
2018-10-27 18:51:54 +0200

    base route removal

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-var base = '/server'
-
 app.use(base + '/', indexRouter)
 app.use(base + '/users', usersRouter)
 
